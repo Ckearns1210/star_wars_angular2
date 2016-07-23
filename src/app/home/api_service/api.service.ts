447bc081d141a1fea19d84a34b4c40f1bf12000e
Colin Kearns <ckearns1210@gmail.com>
2016-07-23 19:57:19 -0400

    commented

--- src/app/home/api_service/api.service.ts
+++ src/app/home/api_service/api.service.ts
@@ -15,10 +15,10 @@ export class ApiService {
     return this.http.get(character.url)
       //flatten out results of first results stream and extract the films, flatMap performs map, but instead of returning an observable, it subscribes to each observable returning the data.
       .flatMap((response: Response) => response.json().films)
-      //flatten out the second request,
+      //flatten out the second request, this request takes the film we extracted and and does a new request for that film, and adds it to the stream as json when it is done
       .flatMap((film: string) => this.http.get(film),
       (_, resp) => resp.json())
-      ////Typescript's object destructuring syntax
+      ////Typescript's object destructuring syntax, takes just the title and release_date from each object coming out of flatMap stream and creates new object
       .map(({title, release_date}) => ({ title, release_date }))
   }
 }
